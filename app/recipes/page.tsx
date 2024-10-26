02567ce4a5cc623065ffd9b1517c36b31d3b84ed
Ahmed Helal <1ahmedhelal1@gmail.com>
2024-10-26 16:26:52 +0300

    #

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -19,7 +19,7 @@ const page: FC<IProps> = ({  }) => {
     <>
     <section>
       <div className="container">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           <RecipeCard/>
           <RecipeCard/>
           <RecipeCard/>
--- components/LatestRecipes.tsx
+++ components/LatestRecipes.tsx
@@ -19,7 +19,7 @@ const LatestRecipes: FC<IProps> = ({  }) => {
     <section>
       <div className="container">
         <SecTitle title="latest recipes" />
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           <RecipeCard/>
           <RecipeCard/>
           <RecipeCard/>
