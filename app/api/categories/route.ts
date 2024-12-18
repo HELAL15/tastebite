21b4e7680f030425786e0bf0c3f376cf2e1434b2
HELAL15 <1ahmedhelal1@gmail.com>
2024-12-18 16:49:49 +0200

    api

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,30 +1,30 @@
-import { client } from '@/lib/mongodb';
+// import { client } from '@/lib/mongodb';
 import { NextResponse } from 'next/server';
 
 
-// export async function GET() {
-//   const data = { message: 'Hello from Categories API' };
-//   return NextResponse.json(data);
-// }
 export async function GET() {
-  try {
-    // Connect to the database
-    await client.connect();
+  const data = { message: 'Hello from Categories API' };
+  return NextResponse.json(data);
+}
+// export async function GET() {
+//   try {
+//     // Connect to the database
+//     await client.connect();
 
     
-    const db = client.db('tastebite'); 
-    const categoriesCollection = db.collection('categories');
+//     const db = client.db('tastebite'); 
+//     const categoriesCollection = db.collection('categories');
 
     
-    const categories = await categoriesCollection.find({}).toArray();
+//     const categories = await categoriesCollection.find({}).toArray();
 
   
-    return NextResponse.json({ success: true, data: categories });
-  } catch (error) {
-    console.error('Error fetching categories:', error);
-    return NextResponse.json({ success: false, message: 'Failed to fetch categories' }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ success: true, data: categories });
+//   } catch (error) {
+//     console.error('Error fetching categories:', error);
+//     return NextResponse.json({ success: false, message: 'Failed to fetch categories' }, { status: 500 });
+//   }
+// }
 
 export async function POST(req: Request) {
   const body = await req.json();
