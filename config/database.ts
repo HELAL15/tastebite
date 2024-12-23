f4adc1e564547a23a4abf4b5058cfb907e1b5ba2
HELAL15 <1ahmedhelal1@gmail.com>
2024-12-23 15:22:53 +0200

    MONGO_URI

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose'
 
-const mongoUri = process.env.MONGO_URI
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://1ahmedhelal1:2qtBgaxXVnMPd505@cluster0.p5a9e.mongodb.net/tastebite'
 if (!mongoUri) {
   throw new Error('Error: MONGO_URI is not defined in environment variables.');
 }
