ad3610b252a16a2438b9d87a7cc7893df42cce87
HELAL15 <1ahmedhelal1@gmail.com>
2024-12-23 16:25:02 +0200

    res method

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,16 +1,16 @@
 
 import connectDB from '@/config/database';
 import categoryModel from '@/models/categoryModel';
-import { NextResponse } from 'next/server';
+
 
 
 export async function GET() {
   try{
     await connectDB()
     const cats = await categoryModel.find()
-    return  NextResponse.json({data:cats , message:'all categories'}, {status: 200});
+    return  Response.json({data:cats , message:'all categories'}, {status: 200});
   }catch(error){
-    return NextResponse.json({ message: 'error ecoured', data: error }, { status: 500 })
+    return Response.json({ message: 'error ecoured', data: error }, { status: 500 })
   }
 }
 
@@ -21,9 +21,9 @@ export async function POST(req: Request) {
     await connectDB()
     const {title, image} = await req.json()
     const cat = await categoryModel.create({title,image})
-    return new Response( JSON.stringify(cat), {status: 201, headers: {'Content-Type': 'application/json'}})
+    return  Response.json( {data:JSON.stringify(cat)}, {status: 201})
   }catch(error){
-    return NextResponse.json({ message: 'error ecoured', data: error })
+    return Response.json({ message: 'error ecoured', data: error })
   }
 
 }
